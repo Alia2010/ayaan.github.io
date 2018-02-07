be1d64c128be665883099917099bd7a8fa85c19c
Ali Ahmad <ali.ahamad.ansari@gmail.com>
2018-02-07 10:45:53 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+	
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Google is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Google is cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
